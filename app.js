dd3984b8163fbe43f65b79987d87e842c1a94b8e
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-11-20 08:40:40 +0100

    Wordle plan

---
+++ app.js
@@ -0,0 +1,7 @@
+// 5 bogstaver i ordet, 6 gæt
+// Grå indikerer at det ikke er i ordet, gul indikerer at det er i ordet, men på fejl plads, grøn indikere at bogstavet er korrekt på pladsen
+// String correct = "MANGO";
+// string guess = "CANDY";
+//Tjek plads 0 fra main string med plads 0 i sub string ?
+// If og else statements ? Loop i gennem hver bogstav ? for(let i = 0; i<5; i++)
+// Brug input kasser i html igen, ligesom med kodeomformer ?
