a85caf081d26f69c06083ad7d0ed20ea86eec9fa
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-11-20 14:16:34 +0100

    Mandag d. 20

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/SbgH9FA6)
 # Wordle
 Skabelon til [Wordle](https://www.nytimes.com/games/wordle/index.html) - [Notion beskrivelse](https://mercantec.notion.site/Wordle-101117d07e79459c8fc5b74e6984516a?pvs=4)
+
+
+skitse
+// Ordliste 
+// 5 bogstaver i ordet, 6 gæt
+// Grå indikerer at det ikke er i ordet, gul indikerer at det er i ordet, men på fejl plads, grøn indikere at bogstavet er korrekt på pladsen
+// String correct = "MANGO";
+// string guess = "CANDY";
+//Tjek plads 0 fra main string med plads 0 i sub string ? ARRAY
+// If og else statements ? Loop i gennem hver bogstav ? for(let i = 0; i<5; i++)
+// Brug input kasser i html igen, ligesom med kodeomformer ?
+// Button til function
+// Wordlist og math random ?
+
+
+
+
+STEP 1: 
+
+Start med at lave en funktion der tjekker for hvert bogstav i et ord.
+
+1. Grå = Er slet ikke i ordet
+2. Gul = Er i ordet, men ikke på den plads
+3. Grøn = Er i ordet og er på den rigtige plads..
+
+(Tjek plads 0 fra main string med plads 0 i sub string ? ARRAY
+// If og else statements ? Loop i gennem hver bogstav ? for(let i = 0; i<5; i++))
+
+STEP 2: Prøv at lav en function med to ord
+
+
+STEP 3: Lav en oversigt over tidligere gæt og placering
+
+
+STEP 4:  Lav en wordlist ud fra "Wordle words txt", hvorfra der bliver valgt et tilfærdigt ord.
+
+
+STEP 5: lav et tastatur
+
+
+STEP 6: Få sat det op i CSS Grid og lav design 5x6 rækker
+
--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-// 5 bogstaver i ordet, 6 gæt
-// Grå indikerer at det ikke er i ordet, gul indikerer at det er i ordet, men på fejl plads, grøn indikere at bogstavet er korrekt på pladsen
-// String correct = "MANGO";
-// string guess = "CANDY";
-//Tjek plads 0 fra main string med plads 0 i sub string ?
-// If og else statements ? Loop i gennem hver bogstav ? for(let i = 0; i<5; i++)
-// Brug input kasser i html igen, ligesom med kodeomformer ?
+let correct = "mango";
+let testWord = "";
+let round = 0;
+
+function checkWord () {
+    for (let i = 0; i<5; i++) {
+        if (correct[i] === testWord[i]) {
+            document.getElementById("r"+ round + "c" + (i)).style.backgroundColor = "green"
+        }
+        else if (testWord[i] === correct[0] || testWord[i] === correct[1] || testWord[i] === correct[2] || testWord[i] === correct[3] || testWord[i] === correct[4]) {
+            document.getElementById("r"+ round + "c" + (i)).style.backgroundColor = "yellow"
+        }
+        else {
+            document.getElementById("r"+ round + "c" + (i)).style.backgroundColor = "grey"
+        }
+    }
+}
+
+function checkValidWord () {
+    testWord = "";
+    console.log(testWord)
+    for (let i=0; i<5; i++) {
+        testWord += document.getElementById("r" + round + "c" + i).value;
+    }
+    if (testWord === correct) {
+        //game end
+        round = 0;
+    }
+    if (testWord.length === 5) { //later test valid dictionary word
+        checkWord ();
+        round++;
+    }
+    else {
+        alert("Not valid?")
+    }
+}
+
+//Lige nu viser den både grøn og gul for a
